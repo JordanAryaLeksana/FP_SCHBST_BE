16d3abcae24d7715b3951e7754f56ce93259305e
JordanAryaLeksana <jordanaryaleksana41@gmail.com>
2024-09-19 18:28:29 +0700

    updates layout

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ import express from 'express'
 
 const app = express()
 
-// PORT=8000
-// OPEN_API_KEY = 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
+
 const PORT = process.env.PORT || 8000
 
 
